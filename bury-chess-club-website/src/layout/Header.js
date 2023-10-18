09179cc59bee1efdecca1e3c857b245501e7c9f7
Joel Hutchinson <hutchy100@live.co.uk>
2023-10-19 00:28:12 +0100

    Simplified header and corresponding css.

--- bury-chess-club-website/src/layout/Header.js
+++ bury-chess-club-website/src/layout/Header.js
@@ -11,49 +11,42 @@ class Header extends React.Component {
     render() {
         return (
             <header>
-                <div className={"hero"}>
-                    <img id={"logo-image"} src={Logo} alt={"Bury Chess Club Logo"} />
-                    <h1 id={"club-name-heading"}>Bury Chess Club</h1>
-                </div>
-                <div id={"navbar"}>
-                    <nav id={"navigation"}>
-                        <ul id={"navigation-list"}>
-                            <li className={"navigation-item"}>
-                                <NavLink
-                                    to="/about"
-                                    className={({ isActive, isPending }) =>
-                                        isPending ? "navigation-link-pending" : isActive ? "navigation-link-active" : "navigation-link"
-                                    }
-                                >
-                                    About Us
-                                </NavLink>
-                            </li>
-                            <li className={"navigation-item"}>
-                                <NavLink
-                                    to="/contact"
-                                    className={({ isActive, isPending }) =>
-                                        isPending ? "navigation-link-pending" : isActive ? "navigation-link-active" : "navigation-link"
-                                    }
-                                >
-                                    Contact
-                                </NavLink>
-                            </li>
-                            <li className={"navigation-item"}>
-                                <NavLink
-                                    to="/competitions"
-                                    className={({ isActive, isPending }) =>
-                                        isPending ? "navigation-link-pending" : isActive ? "navigation-link-active" : "navigation-link"
-                                    }
-                                >
-                                    Club Competitions
-                                </NavLink>
-                            </li>
-                        </ul>
-                    </nav>
-                    <div id={"social-icons"}>
-                        <a className={"social-icon"} href={'https://www.facebook.com/BuryChessClub/'} target={'_blank'}><i className={'fa fa-facebook'}></i></a>
-                        <a className={"social-icon"} href={'https://www.chess.com/club/bury-chess-club'} target={'_blank'}><i className={'fa fa-twitter'}></i></a>
-                    </div>
+                <img id={"logo-image"} src={Logo} alt={"Bury Chess Club Logo"} />
+                <h1 id={"club-name-heading"}>Bury Chess Club</h1>
+                <nav id={"navigation"}>
+                    <ul id={"navigation-list"}>
+                        <li>
+                            <NavLink
+                                to="/about"
+                                className={({ isActive, isPending }) =>
+                                    isPending ? "navigation-link-pending" : isActive ? "navigation-link-active" : "navigation-link"
+                                }
+                            >About Us
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to="/contact"
+                                className={({ isActive, isPending }) =>
+                                    isPending ? "navigation-link-pending" : isActive ? "navigation-link-active" : "navigation-link"
+                                }
+                            >Contact
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to="/competitions"
+                                className={({ isActive, isPending }) =>
+                                    isPending ? "navigation-link-pending" : isActive ? "navigation-link-active" : "navigation-link"
+                                }
+                            >Club Competitions
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+                <div id={"social-icons"}>
+                    <a className={"social-icon"} href={'https://www.facebook.com/BuryChessClub/'} target={'_blank'}><i className={'fa fa-facebook'}></i></a>
+                    <a className={"social-icon"} href={'https://www.chess.com/club/bury-chess-club'} target={'_blank'}><i className={'fa fa-twitter'}></i></a>
                 </div>
             </header>
         );
