aa7304f51f7fa6401cb607834d82f9c90aecf5e9
Joel Hutchinson <hutchy100@live.co.uk>
2023-12-06 14:39:41 +0000

    Added effect and state to facebook timeline component to track height of main panel.

--- bury-chess-club-website/src/components/FacebookTimeline.js
+++ bury-chess-club-website/src/components/FacebookTimeline.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FacebookProvider, Page } from 'react-facebook';
 
 export default function FacebookTimeline() {
+  const [key, setKey] = useState(0);
+  const [mainPanelHeight, setMainPanelHeight] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      const mainPanel = document.getElementById('main-panel');
+      if (mainPanel) {
+        const height = mainPanel.clientHeight;
+        setMainPanelHeight(height);
+      }
+      setKey(prevKey => prevKey + 1);
+    }, 200)
+    
+  }, []);
+
   return (
     <FacebookProvider appId="322305064086840">
-      <Page href={"https://www.facebook.com/BuryChessClub/"} tabs={"timeline"} width={"600"} height={"800"}/>
+      <Page
+        key={key}
+        href={'https://www.facebook.com/BuryChessClub/'}
+        tabs={'timeline'}
+        width={'600'}
+        height={mainPanelHeight}
+      />
     </FacebookProvider>
   );
 }
